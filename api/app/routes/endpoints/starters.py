2a7621575f18642ba852c42a5508e41f53aae485
Sam Russell <samrussell63@gmail.com>
2019-10-26 14:19:18 -0700

    adding a roulette route

--- api/app/routes/endpoints/starters.py
+++ api/app/routes/endpoints/starters.py
@@ -20,6 +20,11 @@ conversation_list_db = {
 }
 
 
+@router.get("/roulette")
+async def get_random_starter():
+    return random.choice(conversation_list_db[random.choice([e.value for e in Topic])])
+
+
 @router.get("/{topic}")
 async def get_conversation_starters(topic: Topic):
     return random.choice(conversation_list_db[topic])
